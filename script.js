2eaf59a8ce8f4fbf7547e7663226fb2f13b8178a
nthanhphong79 <87847256+nthanhphong79@users.noreply.github.com>
2021-07-30 14:54:18 +0700

    travel
    travel in Viet Nam

---
+++ script.js
@@ -0,0 +1,5 @@
+// Menu-bar
+const menubarBtn = document.querySelector('.header-top i')
+menubarBtn.addEventListener("click",function(){
+    document.querySelector('.header-top ul').classList.toggle('active')
+})
